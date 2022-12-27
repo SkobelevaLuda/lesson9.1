bf6bdfd220632674e246c120f0104196a3859d55
Людок <skobeleva.ludmila@yandex.ru>
2022-12-27 03:16:55 +0300

    Первый коммит

---
+++ src/Task4.java
@@ -0,0 +1,28 @@
+public class Task4 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Map<String, List<Integer>> map=new HashMap<>();
+        int mapSize = 5;
+        int listSize = 3;
+        for (int i = 0; i< mapSize; i++) {
+            List<Integer>numbers = new ArrayList<>(listSize);
+            for (int j = 0; j <listSize; j++) {
+                numbers.add(random.nextInt(1000));
+            }
+            map.put(String.valueOf(i),numbers);
+        }
+        System.out.println(map);
+
+        Map<String,Integer> transformerMap = new HashMap<>();
+        for (Map.Entry<String, List<Integer>> entry: map.entrySet()){
+            int sum=0;
+            List<Integer> numbers=entry.getValue();
+            for(Integer nambber: numbers) {
+                sum+=nambber;
+            }
+            transformerMap.put(entry.getKey(),sum);
+
+        }
+        System.out.println(transformerMap);
+    }
+}
